67fe0a510fdbda4623aa6a023689ca31d33d0afb
Ria Cherry <lilyflowerangel@gmail.com>
2023-03-27 04:03:23 +0530

    Added a script that applies properties to lcd panels, from their custom data.

--- PBScripts/AutoCloseDoors/Program.cs
+++ PBScripts/AutoCloseDoors/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Sandbox.ModAPI.Ingame;
-using System;
 using System.Collections.Generic;
 
 namespace PBScripts.AutoCloseDoors
@@ -11,40 +10,16 @@ namespace PBScripts.AutoCloseDoors
     /// </summary>
     public class Program : MyGridProgram
     {
-        /// <summary>
-        /// The constructor, called only once every session and
-        /// always before any other method is called. Use it to
-        /// initialize your script.
-        ///
-        /// The constructor is optional and can be removed if not
-        /// needed.
-        ///
-        /// It's recommended to set RuntimeInfo.UpdateFrequency
-        /// here, which will allow your script to run itself without a
-        /// timer block.
-        /// </summary>
+        private byte _delayCount = 0;
+        private const byte _delayMultiplier = 2;
+        private readonly List<IMyDoor> _openDoors = new List<IMyDoor>();
+        private readonly List<IMyDoor> _lastDoors = new List<IMyDoor>();
+
         public Program()
         {
             Runtime.UpdateFrequency = UpdateFrequency.Update100;
         }
 
-        private byte _delayCount = 0;
-        private const byte _delayMultiplier = 2;
-
-        private readonly List<IMyDoor> _openDoors = new List<IMyDoor>();
-        private readonly List<IMyDoor> _lastDoors = new List<IMyDoor>();
-
-        /// <summary>
-        /// The main entry point of the script, invoked every time
-        /// one of the programmable block's Run actions are invoked,
-        /// or the script updates itself. The updateSource argument
-        /// describes where the update came from.
-        ///
-        /// The method itself is required, but the arguments above
-        /// can be removed if not needed.
-        /// </summary>
-        /// <param name="argument"></param>
-        /// <param name="updateSource"></param>
         public void Main(string argument, UpdateType updateSource)
         {
             _delayCount++;
@@ -67,14 +42,6 @@ namespace PBScripts.AutoCloseDoors
             _lastDoors.AddRange(_openDoors);
         }
 
-        /// <summary>
-        /// Called when the program needs to save its state. Use
-        /// this method to save your state to the Storage field
-        /// or some other means.
-        ///
-        /// This method is optional and can be removed if not
-        /// needed.
-        /// </summary>
         public void Save()
         { }
     }
---
+++ PBScripts/LcdApplyProperty/Program.cs
@@ -0,0 +1,117 @@
+﻿using Sandbox.ModAPI.Ingame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using VRageMath;
+
+namespace PBScripts.LcdApplyProperty
+{
+    internal class Program : MyGridProgram
+    {
+        // Run
+
+        private bool _firstRun = true;
+        private const bool _autorun = true;
+        private const byte _delayMultiplier = 60;
+        private byte _delayCount = 0;
+
+        public Program()
+        {
+            if (_autorun)
+                Runtime.UpdateFrequency = UpdateFrequency.Update100;
+        }
+
+        public void Main()
+        {
+            _delayCount++;
+            if (_delayCount >= _delayMultiplier)
+            {
+                DoRoutine();
+                _delayCount = 0;
+            }
+            else if (_firstRun)
+            {
+                DoRoutine();
+                _firstRun = false;
+            }
+        }
+
+        // Routine
+
+        private readonly List<IMyTextPanel> _panels = new List<IMyTextPanel>();
+
+        public void DoRoutine()
+        {
+            // Enumerate:
+            // We don't want to overload per-frame processing
+            // So we'll process one block per permitted cycle
+            if (_panels.Any())
+            {
+                IMyTextPanel panel = _panels[0];
+                _panels.RemoveAt(0);
+                SetOne(panel);
+                return;
+            }
+
+            // else get all for next enumeration (this condition shouldn't be heavy)
+            GridTerminalSystem.GetBlocksOfType(_panels, x => !string.IsNullOrEmpty(x.CustomData));
+        }
+
+        // Process one block
+
+        private string[] _customData;
+        private readonly List<string> _lcdproperties = new List<string>();
+
+        private void SetOne(IMyTextPanel panel)
+        {
+            _customData = panel.CustomData
+                    .Split(new string[] { "\r\n", "\r", "\n", ";" }, StringSplitOptions.RemoveEmptyEntries);
+
+            ExtractProperties(_customData);
+
+            if (_lcdproperties.Count > 0)
+                SetFontColor(panel, _lcdproperties[0]);
+            if (_lcdproperties.Count > 1)
+                SetBackgroundColor(panel, _lcdproperties[1]);
+        }
+
+        private void ExtractProperties(string[] elements)
+        {
+            bool read = false;
+            foreach (string element in elements)
+            {
+                if (element.ToLowerInvariant().Equals("lcdautoapply{"))
+                    read = true;
+                else if (element.ToLowerInvariant().Equals("}"))
+                    read = false;
+                else if (read)
+                    _lcdproperties.Add(element);
+            }
+        }
+
+        private void SetFontColor(IMyTextPanel panel, string colorHex)
+        {
+            try { panel.FontColor = ToColor(colorHex); }
+            catch { Echo($"Failed: FontColor-{colorHex}"); }
+        }
+
+        private void SetBackgroundColor(IMyTextPanel panel, string colorHex)
+        {
+            try { panel.BackgroundColor = ToColor(colorHex); }
+            catch { Echo($"Failed: BackgroundColor-{colorHex}"); }
+        }
+
+        private static Color ToColor(string hex)
+        {
+            return new Color(
+                byte.Parse(hex.Substring(1, 2), System.Globalization.NumberStyles.HexNumber),
+                byte.Parse(hex.Substring(3, 2), System.Globalization.NumberStyles.HexNumber),
+                byte.Parse(hex.Substring(5, 2), System.Globalization.NumberStyles.HexNumber));
+        }
+
+        // Storage
+
+        public void Save()
+        { }
+    }
+}
